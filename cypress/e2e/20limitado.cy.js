a18ca0ab8de1817115573987bb421c9ffbcf115e
MaiaraMuller <137832938+maiaramuller@users.noreply.github.com>
2024-12-12 22:55:34 -0300

    QA Store Desafio

---
+++ cypress/e2e/20limitado.cy.js
@@ -0,0 +1,33 @@
+describe("Adicionar cupom 20LIMITADO e verificar valores", () => {
+  it("Deve adicionar o cupom e constar que o cupom é inválido", () => {
+    cy.visit(
+      "https://qastoredesafio.lojaintegrada.com.br/categoria/15610609.html"
+    );
+
+    cy.get("div.listagem-item.prod-id-118475045")
+      .find('a[title="Adicionar produto ao carrinho"]')
+      .click({ force: true });
+
+    cy.get('input[name="cupom"]').type("20LIMITADO");
+
+    cy.contains("button", "Usar cupom").should("be.visible").click();
+
+    cy.get("div.alert.alert-danger.alert-geral")
+      .should("be.visible")
+      .and("contain.text", "Cupom não encontrado");
+
+    cy.get("strong.valor-subtotal")
+      .invoke("text")
+      .then((subtotalText) => {
+        cy.get("strong.valor-total")
+          .invoke("text")
+          .then((totalText) => {
+            const subtotal = subtotalText
+              .trim()
+              .replace(/\u00a0|R\$|\s|,/g, "");
+            const total = totalText.trim().replace(/\u00a0|R\$|\s|,/g, "");
+            expect(subtotal).to.eq(total);
+          });
+      });
+  });
+});
